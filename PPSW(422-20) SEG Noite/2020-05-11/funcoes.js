bd2aeeb7c0091c0daf2c0942bca997ec17b9c1b5
programandocomjoao <46997033+programandocomjoao@users.noreply.github.com>
2020-05-12 10:38:42 -0300

    Add files via upload

---
+++ PPSW(422-20) SEG Noite/2020-05-11/funcoes.js
@@ -0,0 +1,36 @@
+function calcularDelta() {
+	var A = Number(document.getElementById('A').value);
+	var B = Number(document.getElementById('B').value);
+	var C = Number(document.getElementById('C').value);
+
+	var delta = Math.pow(B, 2) - 4 * A * C;
+
+	document.getElementById('delta').value = delta;
+}
+
+function calcularComprimento() {
+	var raio = Number(document.getElementById('raio').value);
+
+	var C =  2 * Math.PI * raio;
+	C = C.toFixed(2);
+
+	document.getElementById('C').value = C;
+}
+
+function calcularArea() {
+	var raio = Number(document.getElementById('raio').value);
+
+	var A = Math.PI * Math.pow(raio, 2);
+	A = A.toFixed(2);
+
+	document.getElementById('A').value = A;
+}
+
+function calcularVolume() {
+	var raio = Number(document.getElementById('raio').value);
+
+	var V = 0.75 * Math.PI * Math.pow(raio, 3);
+	V = V.toFixed(2);
+
+	document.getElementById('V').value = V;
+}
